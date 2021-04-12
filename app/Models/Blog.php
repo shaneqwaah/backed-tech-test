bfdf37fb00c24c5b15142547e6b4979e323b5668
Chris <chris@builtypixel.com>
2021-04-12 12:52:52 +0100

    moar udpates

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,14 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Blog extends Model
 {
     use HasFactory;
 
     protected $casts = [
-        'content' => 'array'
+        'content' => 'array',
     ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
